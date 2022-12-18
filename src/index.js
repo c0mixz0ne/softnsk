76e70cb160d22f1a283a44adbb894580491e11cf
Антон <anton@MacBook-Air-user.local>
2022-12-18 19:07:25 +0700

    add files

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import "./fonts/fonts.scss";
+import "./_cleanup.scss";
+import "./_main.scss";
+
+console.log("working...");
+
+//Get form data
+document.querySelector("form").addEventListener("submit", (e) => {
+  const data = Object.fromEntries(new FormData(e.target).entries());
+  console.log(data);
+  debugger;
+  //debugger for check
+});
+//Dropdown
+let button = document.querySelector(".form__select-button");
+let dropDown = document.querySelector(".form__select-dropdown");
+let listDropDown = document.querySelectorAll(".form__select-item");
+let dropDownInput = document.querySelector('[name="select"]');
+
+listDropDown.forEach((item) => {
+  item.addEventListener("click", () => {
+    button.innerText = item.innerText;
+    dropDownInput.value = item.dataset.value;
+    button.classList.toggle("form__select-button_open");
+    dropDown.classList.toggle("form__select-dropdown_open");
+  });
+});
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  button.classList.toggle("form__select-button_open");
+  dropDown.classList.toggle("form__select-dropdown_open");
+});
+document.addEventListener("click", (e) => {
+  if (e.target.closest(".list")) return;
+
+  button.classList.remove("form__select-button_open");
+  dropDown.classList.remove("form__select-dropdown_open");
+});
+//Range
+let range = document.querySelector(".form__range-input");
+let rangeText = document.querySelector(".form__range-value");
+// let rangeInput = document.querySelector('[name="range"]');
+range.addEventListener('input', () => {
+    rangeText.innerText = `${range.value} %`
+    // rangeInput.value = range.value
+})
+//Burger
+let burger = document.querySelector('.burger');
+let menu = document.querySelector('.navigation');
+burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_open');
+    menu.classList.toggle('navigation_open');
+
+})
+window.addEventListener('resize', () => {
+    burger.classList.remove('burger_open');
+    menu.classList.remove('navigation_open');
+})
+window.addEventListener('scroll', () => {
+    burger.classList.remove('burger_open');
+    menu.classList.remove('navigation_open');
+})
+//Scroll
+let menuItems = document.querySelectorAll('.navigation__link')
+let form = document.querySelector('.form')
+let yOffset = 50;
+let coords = form.getBoundingClientRect().top + window.pageYOffset - yOffset;
+menuItems.forEach(menuItem => {
+  // if (menuItem.href === ?) {
+  //  do something
+  // }
+  menuItem.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.scrollTo({
+      top: coords,
+      behavior: "smooth"
+  })
+  })
+});
